57aced4958f49628fee8364a30d099610ed90c4d
ignat <buyfng@gmail.com>
2018-02-20 20:05:28 +0300

    actually compare files in the binary

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import program from 'commander';
 import version from '../../package.json';
+import gendiff from '../';
 
 program
   .version(version)
   .description('Compares two files, shows how they differ.')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'output format')
+  .action((firstConfig, secondConfig) => console.log(gendiff(firstConfig, secondConfig)))
   .parse(process.argv);
