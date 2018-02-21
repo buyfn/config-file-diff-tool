afa92f9307d433c98e24e89f60625e4095709248
ignat <buyfng@gmail.com>
2018-02-21 16:05:20 +0300

    simplify union

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import fs from 'fs';
-import _ from 'lodash';
+import array from 'lodash/array';
 
 const gendiff = (pathToFile1, pathToFile2) => {
   const fileContent1 = JSON.parse(fs.readFileSync(pathToFile1));
   const fileContent2 = JSON.parse(fs.readFileSync(pathToFile2));
 
-  const keys = _.union([...Object.keys(fileContent1), ...Object.keys(fileContent2)]);
+  const keys = array.union(Object.keys(fileContent1), Object.keys(fileContent2));
 
   const diff = keys.reduce((acc, key) => {
     if (fileContent1[key] && fileContent2[key]) {
@@ -20,9 +20,7 @@ const gendiff = (pathToFile1, pathToFile2) => {
     return [...acc, `  + ${key}: ${fileContent2[key]}`];
   }, []);
 
-  const diffString = `{\n${diff.join('\n')}\n}`;
-
-  return diffString;
+  return `{\n${diff.join('\n')}\n}`;
 };
 
 export default gendiff;
