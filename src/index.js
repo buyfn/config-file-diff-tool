7fe1586a3552d311da106137c13324774b2b67d3
ignat <buyfng@gmail.com>
2018-02-22 13:56:29 +0300

    refactor parse

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
 import fs from 'fs';
+import path from 'path';
 import array from 'lodash/array';
 import jsyaml from 'js-yaml';
 import ini from 'ini';
 
-const read = (pathToFile) => {
-  const format = array.last(pathToFile.split('.'));
-  const content = fs.readFileSync(pathToFile, 'utf8');
+const read = pathToFile => fs.readFileSync(pathToFile, 'utf8');
 
-  return { content, format };
-};
-
-const parse = (file) => {
+const parse = (text, format) => {
   const parserMap = {
-    json: JSON.parse,
-    yaml: jsyaml.load,
-    ini: ini.parse,
+    '.json': JSON.parse,
+    '.yaml': jsyaml.load,
+    '.ini': ini.parse,
   };
 
-  return parserMap[file.format](file.content);
+  return parserMap[format](text);
 };
 
 const gendiff = (pathToFile1, pathToFile2) => {
-  const fileContent1 = parse(read(pathToFile1));
-  const fileContent2 = parse(read(pathToFile2));
+  const fileContent1 = parse(read(pathToFile1), path.extname(pathToFile1));
+  const fileContent2 = parse(read(pathToFile2), path.extname(pathToFile2));
 
   const keys = array.union(Object.keys(fileContent1), Object.keys(fileContent2));
 
